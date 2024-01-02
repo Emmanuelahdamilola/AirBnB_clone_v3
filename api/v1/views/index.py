718f6d4abfd59121ed630b85cc2b9824078bf6f8
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2024-01-01 17:18:30 -0800

    modified

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-
-
 def status():
-    """Return JSON status: OK"""
-    return jsonify({"status": "OK"})
+    """
+    Return JSON status: OK
 
+    This route handler is responsible for handling HTTP GET requests to the
+    '/status' endpoint. returns a JSON response indicating the status as "OK".
+    """
+    return jsonify({"status": "OK"})
